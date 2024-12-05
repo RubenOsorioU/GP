dd22aed805dac7d75b9b5a41278023947c4ed0b5
Antonio de Jesus Perez Vicencio <71039689+Antoniopv7@users.noreply.github.com>
2024-12-05 19:05:02 -0300

    Commit 28 ChangePassword funcional

--- ViewModels/ChangePasswordViewModel.cs
+++ ViewModels/ChangePasswordViewModel.cs
@@ -1,23 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Gestion_Del_Presupuesto.ViewModels
+public class ChangePasswordViewModel
 {
-    public class ChangePasswordViewModel
-    {
-        [Required(ErrorMessage = "La contraseña actual es obligatoria.")]
-        [DataType(DataType.Password)]
-        [Display(Name = "Contraseña Actual")]
-        public string CurrentPassword { get; set; }
+    [Required(ErrorMessage = "La contraseña actual es requerida.")]
+    [DataType(DataType.Password)]
+    public string CurrentPassword { get; set; }
 
-        [Required(ErrorMessage = "La nueva contraseña es obligatoria.")]
-        [StringLength(100, ErrorMessage = "La {0} debe tener al menos {2} y un máximo de {1} caracteres.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Nueva Contraseña")]
-        public string NewPassword { get; set; }
+    [Required(ErrorMessage = "La nueva contraseña es requerida.")]
+    [DataType(DataType.Password)]
+    [StringLength(100, ErrorMessage = "La {0} debe tener al menos {2} caracteres.", MinimumLength = 6)]
+    public string NewPassword { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirmar Nueva Contraseña")]
-        [Compare("NewPassword", ErrorMessage = "La nueva contraseña y la confirmación no coinciden.")]
-        public string ConfirmPassword { get; set; }
-    }
+    [DataType(DataType.Password)]
+    [Compare("NewPassword", ErrorMessage = "La nueva contraseña y la confirmación no coinciden.")]
+    public string ConfirmPassword { get; set; }
 }
