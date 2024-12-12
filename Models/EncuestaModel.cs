db6fb1ff7cbe8111788cab36cd0fac8c5533f0e9
Antonio de Jesus Perez Vicencio <71039689+Antoniopv7@users.noreply.github.com>
2024-12-11 21:34:09 -0300

    Commit 32 cambio de contra cambiado, y encuetas actualizado

--- Models/EncuestaModel.cs
+++ Models/EncuestaModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 
 namespace Gestion_Del_Presupuesto.Models
 {
@@ -12,14 +13,13 @@ namespace Gestion_Del_Presupuesto.Models
         public string Tipo { get; set; } // "Estudiante a Campo Clínico" o "Campo Clínico a Estudiante"
 
         [Required]
-        [Display(Name = "Comentarios")]
-        [StringLength(1000, ErrorMessage = "Los comentarios no pueden exceder los 1000 caracteres.")]
-        public string Comentarios { get; set; }
+        [Display(Name = "Archivo de Encuesta")]
+        public string ArchivoRuta { get; set; } // Ruta del archivo subido
 
         [Required]
         [Display(Name = "Puntuación")]
         [Range(1, 5, ErrorMessage = "La puntuación debe estar entre 1 y 5.")]
-        public int Puntuacion { get; set; }
+        public int Puntuacion { get; set; } // Puntuación para la encuesta
 
         [Required]
         [Display(Name = "Fecha de Encuesta")]
--- ViewModels/ChangePasswordViewModel.cs
+++ ViewModels/ChangePasswordViewModel.cs
@@ -1,17 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-public class ChangePasswordViewModel
+namespace Gestion_Del_Presupuesto.Models
 {
-    [Required(ErrorMessage = "La contraseña actual es requerida.")]
-    [DataType(DataType.Password)]
-    public string CurrentPassword { get; set; }
+    public class ChangePasswordViewModel
+    {
+        [Required(ErrorMessage = "La contraseña actual es requerida.")]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
 
-    [Required(ErrorMessage = "La nueva contraseña es requerida.")]
-    [DataType(DataType.Password)]
-    [StringLength(100, ErrorMessage = "La {0} debe tener al menos {2} caracteres.", MinimumLength = 6)]
-    public string NewPassword { get; set; }
+        [Required(ErrorMessage = "La nueva contraseña es requerida.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "La {0} debe tener al menos {2} caracteres.", MinimumLength = 6)]
+        public string NewPassword { get; set; }
 
-    [DataType(DataType.Password)]
-    [Compare("NewPassword", ErrorMessage = "La nueva contraseña y la confirmación no coinciden.")]
-    public string ConfirmPassword { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "La nueva contraseña y la confirmación no coinciden.")]
+        public string ConfirmPassword { get; set; }
+    }
 }
